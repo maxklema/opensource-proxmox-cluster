fbd940f0d4ab0a96a3f0bcc772581187e8bca252
root <root@ssh-jump-host.mie.local>
2025-06-14 00:11:55 +0000

    Simple Script for direct ssh via ssh jump host without proxmox host

---
+++ ssh/directSSH.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\n\nMedical Informatics Engineering Container Cluster\n"
+
+echo "Welcome $USER. Let's SSH into your Application."
+read -e -p "Enter your appname: " APPNAME
+echo "Attempting to SSH into $appname.mie.local..."
+
+TARGET_HOST="${APPNAME}.internal"
+
+# Forward the SSH session to target container (host)
+exec ssh -o StrictHostKeyChecking=no -tt "${USER}@${TARGET_HOST}"
